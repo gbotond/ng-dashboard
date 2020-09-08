b2f65d4e048dbd36b0f41005b86d9628116e0673
Botond Gorfol <botond.gorfol@gmail.com>
2020-09-08 21:59:52 +0200

    Rename app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'NgDashboard';
-  subtitle = 'Business Intelligence Dashboard';
+  title = 'NBA Stats';
+  subtitle = 'NBA Statistics Dashboard';
 }
