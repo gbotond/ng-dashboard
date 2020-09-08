e3a99fd06f68f8c4f734c6e964428455bdec3c20
Botond Gorfol <botond.gorfol@gmail.com>
2020-09-08 21:59:36 +0200

    adapt routes to rename of gamecomponent

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { GameStatisticsComponent } from './app/game-statistics/game-statistics.component';
-import { GamesComponent } from './app/games/games.component';
+import { GameComponent } from './app/game/game.component';
 import { SystemHealthComponent } from './app/system-health/system-health.component';
 
 export const appRoutes: Routes = [
     { path: 'stats', component: GameStatisticsComponent },
-    { path: 'games', component: GamesComponent },
+    { path: 'game', component: GameComponent },
     { path: 'health', component: SystemHealthComponent },
 
     { path: '', redirectTo: '/stats', pathMatch: 'full' }
